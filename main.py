e256d9cbfb8fefae4dcfaa27012f3d6eb21fe696
Mdfar <41281837+Mdfar@users.noreply.github.com>
2024-02-24 02:39:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import os
 import time
 import pandas as pd
 
-options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
+chrome_options = Options();
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=options,service=ChromeService(ChromeDriverManager().install()))
 
 username = 'gudjong'
